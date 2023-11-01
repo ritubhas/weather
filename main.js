5917b23e615f3be388b1a020ed83230cbd59d337
Ritika Nagpal <rbhas03@gmail.com>
2023-10-31 18:57:01 -0700

    Third Commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// Step 1: Define cities array
+let cities = [
+    {
+      name: "Benbrook, TX",
+      latitude: 32.6732,
+      longitude: -97.4606,
+    },
+    {
+      name: "Miami, FL",
+      latitude: 25.758844,
+      longitude: -80.188728
+    },
+    {
+      name: "New York, NY",
+      latitude: 40.731631,
+      longitude: -74.005981,
+    },
+    {
+      name: "Las Vegas, NV",
+      latitude: 36.182547,
+      longitude: -115.142056,
+    },
+    {
+      name: "Grand Rapids, MI",
+      latitude: 42.957836,
+      longitude: -85.660092,
+    },
+    {
+      name: "San Antonio, TX",
+      latitude: 29.450993,
+      longitude: -98.552896,
+    },
+    {
+      name: "New Orleans, LA",
+      latitude: 29.941305,
+      longitude: -90.083081,
+    },
+    // Add more cities here
+  ];
+  
